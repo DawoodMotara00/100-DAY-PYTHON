066d6537ce659b95e02132b1d594c4272cc85a7a
dawood motara <us2182623@unimail.hud.ac.uk>
2023-02-14 12:23:14 +0000

    day 5 complete

--- passwordgen.py
+++ passwordgen.py
@@ -15,9 +15,6 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 # Hard Level - Order of characters randomised:
 # e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
 
-
-# #Eazy Level - Order not randomised:
-# e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 random_letters = []
 for i in range(0, nr_letters):
     a = random.randrange(len(letters))
@@ -40,7 +37,38 @@ for i in range(0, nr_numbers):
 print(random_numbers)
 
 pa = random_letters + random_symbols + random_numbers
+random.shuffle(pa)
 password = ""
 for i in pa:
     password += i
 print(password)
+
+
+# #Eazy Level - Order not randomised:
+# e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+# random_letters = []
+# for i in range(0, nr_letters):
+#     a = random.randrange(len(letters))
+#     letter = letters[a]
+#     random_letters += [letter]
+# print(random_letters)
+#
+# random_symbols = []
+# for i in range(0, nr_symbols):
+#     a = random.randrange(len(symbols))
+#     symbol = symbols[a]
+#     random_symbols += [symbol]
+# print(random_symbols)
+#
+# random_numbers = []
+# for i in range(0, nr_numbers):
+#     a = random.randrange(len(numbers))
+#     number = numbers[a]
+#     random_numbers += [number]
+# print(random_numbers)
+#
+# pa = random_letters + random_symbols + random_numbers
+# password = ""
+# for i in pa:
+#     password += i
+# print(password)
